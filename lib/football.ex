08ecb9b94a64076a83cc995960061acaea4dd149
Tres Trantham <tres@trestrantham.com>
2014-06-14 01:33:46 -0400

    Add Application and Supervisor

--- lib/football.ex
+++ lib/football.ex
@@ -1,2 +1,20 @@
 defmodule Football do
+  use Application
+
+  def start(_type, _args) do
+    Football.Supervisor.start_link
+  end
+end
+
+defmodule Football.Supervisor do
+  use Supervisor
+
+  def start_link do
+    :supervisor.start_link({ :local, __MODULE__ }, __MODULE__, [])
+  end
+
+  def init([]) do
+    tree = [worker(Repo, [])]
+    supervise(tree, strategy: :one_for_all)
+  end
 end
--- mix.exs
+++ mix.exs
@@ -16,6 +16,7 @@ defmodule Football.Mixfile do
   # Type `mix help compile.app` for more information
   def application do
     [
+      mod: { Football, [] },
       applications: [
         :postgrex,
         :ecto
